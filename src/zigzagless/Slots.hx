d05c98c9c85904749328e50c6808823f787a8d86
ze0nni <ze0nni@gmail.com>
2019-11-07 20:56:23 +0300

    Pass 'testRemoveFirstElement2Times'

--- src/zigzagless/Slots.hx
+++ src/zigzagless/Slots.hx
@@ -10,7 +10,23 @@ class Slots {
         var size = mediator.getSize();
         var newSize = size - 1;
         
+        var rowsCount = Math.ceil(size / columnsCount);
+
+        var holeX = index % columnsCount;
+        var holeY = Std.int(index / columnsCount);
+
         mediator.remove(index);
+
+        // move hole bottom 
+        for (y in holeY...rowsCount) {
+            var to = y * columnsCount + holeX;
+            var from = to + columnsCount;
+            if (from >= size) {
+                break;
+            }
+            mediator.move(from, to);
+            holeY = y;
+        }
 
         mediator.resize(newSize);
     }
