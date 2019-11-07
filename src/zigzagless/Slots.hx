1d2827b22a66d40e3240f5c1777ccef303af78da
ze0nni <ze0nni@gmail.com>
2019-11-07 22:15:51 +0300

    Pass all exists tests

--- src/zigzagless/Slots.hx
+++ src/zigzagless/Slots.hx
@@ -11,6 +11,8 @@ class Slots {
         var newSize = size - 1;
         
         var rowsCount = Math.ceil(size / columnsCount);
+        var lastRow = rowsCount - 1;
+        var lastRowSize = size % columnsCount;
 
         var holeX = index % columnsCount;
         var holeY = Std.int(index / columnsCount);
@@ -27,6 +29,24 @@ class Slots {
             mediator.move(from, to);
             holeY = y + 1;
         }
+        
+        if (holeY != lastRow) {
+            // move row element to right
+            var row0 = holeY * columnsCount;
+            while (holeX > 0) {
+                holeX -= 1;
+                
+                var from = row0 + holeX;
+                var to = from + 1;
+
+                mediator.move(from, to);
+            }
+
+            // Pop element form bottom row
+            var nextRow0 = row0 + columnsCount;
+            if (nextRow0 < size) {
+                mediator.move(nextRow0, row0);
+            }
         }
 
         // if last row reached just move elements to left
